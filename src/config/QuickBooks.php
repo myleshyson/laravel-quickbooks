bc3e844ccb110f66edc0b424081a7e082d260ef9
spuds10 <mylesshyson@gmail.com>
2016-12-12 13:36:09 -0500

    updated config file

--- src/config/QuickBooks.php
+++ src/config/QuickBooks.php
@@ -1,14 +1,17 @@
 <?php
 
-return [
 
-	'dsn' => env('QB_DSN'),
-	'encryption_key' => env('QB_ENCRYPTION_KEY'),
-	'oauth_consumer_key' => env('QB_OAUTH_CONSUMER_KEY'),
-	'oauth_consumer_secret' => env('QB_OAUTH_CONSUMER_SECRET'),
-	'quickbooks_oauth_url' => env('QB_OAUTH_URL'),
-	'quickbooks_success_url' => env('QB_SUCCESS_URL'),
-	'the_username' => env('QB_USERNAME'),
-	'the_tenant' => env('QB_TENANT')
+
+return [
+    'dsn' => env('DB_CONNECTION').'i://'.env('DB_USERNAME').':'.env('DB_PASSWORD').'@'.env('DB_HOST').'/'.env('DB_DATABASE'),
+    'encryption_key' => env('APP_KEY'),
+    'sandbox' => env('QBO_SANDBOX'),
+    'token' => env('QB_TOKEN'),
+    'oauth_consumer_key' => env('QB_OAUTH_CONSUMER_KEY'),
+    'oauth_consumer_secret' => env('QB_OAUTH_CONSUMER_SECRET'),
+    'quickbooks_oauth_url' => env('QB_OAUTH_URL'),
+    'quickbooks_success_url' => env('QB_SUCCESS_URL'),
+    'the_username' => env('QB_USERNAME'),
+    'the_tenant' => env('QB_TENANT')
 
 ];
