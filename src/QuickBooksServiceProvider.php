bb522ef78083f3985053983d8a0b2182613eec0d
spuds10 <mylesshyson@gmail.com>
2016-12-16 15:11:58 -0500

    fixed customer bug. Deleted unnecessary files

--- src/QuickBooksServiceProvider.php
+++ src/QuickBooksServiceProvider.php
@@ -13,16 +13,13 @@ class QuickBooksServiceProvider extends ServiceProvider
 
     public function register()
     {
-        include __DIR__.'/routes.php';
-        $this->app->make('Myleshyson\LaravelQuickBooks\QuickbooksController');
-
         $this->app->bind('account', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Account');
         $this->app->bind('bill', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Bill');
         $this->app->bind('billpayment', 'Myleshyson\LaravelQuickBooks\Services\Accounting\BillPayment');
         $this->app->bind('class', 'Myleshyson\LaravelQuickBooks\Services\Accounting\QB_Class');
         $this->app->bind('connection', 'Myleshyson\LaravelQuickBooks\Services\Connection');
         $this->app->bind('creditmemo', 'Myleshyson\LaravelQuickBooks\Services\Accounting\CreditMemo');
-        $this->app->bind('customer', 'Myleshyson\LaravelQuickBooks\Services\Customer');
+        $this->app->bind('customer', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Customer');
         $this->app->bind('department', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Department');
         $this->app->bind('employee', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Employee');
         $this->app->bind('estimate', 'Myleshyson\LaravelQuickBooks\Services\Accounting\Estimate');
